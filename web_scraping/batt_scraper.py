a08294e6fe4f4a15d865b3e8785b8f1819a50987
IMMZEK <47085903+IMMZEK@users.noreply.github.com>
2022-10-15 21:28:52 -0500

    Changes made to batt_scraper.py:
    Pushes validated links to a CSV file.
    
    Added missing TODO items for CSV and Webscraping.

--- web_scraping/batt_scraper.py
+++ web_scraping/batt_scraper.py
@@ -1,4 +1,6 @@
 import selenium
+import pandas as pd
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,9 +13,6 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("--ignore-certificate-errors-spki-list")
 chrome_options.add_argument('--ignore-ssl-errors')
-chrome_options.add_argument('--ignore-certificate-errors')
-
-
 
 s = Service(ChromeDriverManager().install()) # manages chromedriver
 driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
@@ -27,15 +26,30 @@ article_elems = driver.find_elements(By.CLASS_NAME, 'tnt-headline')
 # article_links will be a list of links to articles found on the main search page
 article_links = [art_elem.find_element(By.TAG_NAME, 'a').get_attribute('href') for art_elem in article_elems]
 
+#CSV FILE
+article_linksCSV = open('web_scraping/links.csv', 'w') # open CSV reader
+writer = csv.writer(article_linksCSV) # open CSV writer
+# writer.writerow(article_links) #writes separated by commas
+
+#WEBSCRAPING TODO:
+#Remove top jobs from the search
+#Look through order (change website settings)
+
+#CSV TODO:
+#Read current csv file before appending new link and check that its nota already there]
+#if not there append
+#append most recent at bottom ---- CSV: oldest at top and newest at bottom
 
 
 for link in article_links:
     try:
         print(link)
         driver.get(link) # jump to the link
-
+        writer.writerow([link]) # write link to CSV by new line
     except:
         print("Unable to Find Link")
     
+#Close the CSV file
+article_linksCSV.close()
 
 driver.quit()
