bd2495b57dc160d7809df23477fc49a5639e43d5
aum1 <palande.aum@gmail.com>
2022-10-18 16:24:15 -0500

    Changes to scraper, specifically added df and checked if already in file

--- web_scraping/batt_scraper.py
+++ web_scraping/batt_scraper.py
@@ -18,7 +18,7 @@ s = Service(ChromeDriverManager().install()) # manages chromedriver
 driver = webdriver.Chrome(service=s, chrome_options=chrome_options)
 
 # tells bot to go to this link 
-driver.get("https://www.thebatt.com/search/?l=25&sort=relevance&f=html&t=article%2Cvideo%2Cyoutube%2Ccollection&app=editorial&nsa=eedition&q=")
+driver.get("https://www.thebatt.com/search/?l=50&sort=relevance&f=html&t=article%2Cvideo%2Cyoutube%2Ccollection&app=editorial&nsa=eedition&q=")
 
 # getting all articles by the class name - on the battalion, all articles on the search have the class name "tnt-headline"
 article_elems = driver.find_elements(By.CLASS_NAME, 'tnt-headline')
@@ -27,8 +27,14 @@ article_elems = driver.find_elements(By.CLASS_NAME, 'tnt-headline')
 article_links = [art_elem.find_element(By.TAG_NAME, 'a').get_attribute('href') for art_elem in article_elems]
 
 #CSV FILE
-article_linksCSV = open('web_scraping/links.csv', 'w') # open CSV reader
-writer = csv.writer(article_linksCSV) # open CSV writer
+# article_linksCSV = open('web_scraping/links.csv', 'w') # open CSV reader
+# article_linksCSV = open('links.csv', 'w')
+
+# Using a pandas dataframe instead
+df = pd.read_csv("links.csv", names=['links'])
+
+
+# writer = csv.writer(article_linksCSV) # open CSV writer
 # writer.writerow(article_links) #writes separated by commas
 
 #WEBSCRAPING TODO:
@@ -39,17 +45,24 @@ writer = csv.writer(article_linksCSV) # open CSV writer
 #Read current csv file before appending new link and check that its nota already there]
 #if not there append
 #append most recent at bottom ---- CSV: oldest at top and newest at bottom
+# Fix issue with the multiple headers
 
+previousLinks = df['links'].values.tolist()[1:]
 
 for link in article_links:
     try:
-        print(link)
-        driver.get(link) # jump to the link
-        writer.writerow([link]) # write link to CSV by new line
+        # A better optimization may be to reverse it, and append to the top
+        # Assuming its linear search, it would be faster
+        if (not (link in previousLinks)): 
+            df.loc[len(df.index)] = [link]
+        # driver.get(link) # jump to the link
+        # writer.writerow([link]) # write link to CSV by new line
     except:
         print("Unable to Find Link")
     
 #Close the CSV file
-article_linksCSV.close()
+# article_linksCSV.close()
+# print(previousLinks)
+df.to_csv('links.csv') # TODO: Change to correct file location
 
 driver.quit()
