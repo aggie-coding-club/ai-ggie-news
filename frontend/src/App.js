c7af9b088fafa536b701cf8a4af28342acd4ec5b
alimmomin270@gmail.com <53840493+AlimMomin3735@users.noreply.github.com>
2022-10-20 19:20:06 -0500

    SignUpPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,15 +17,8 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ? 
-          <h3>{getMessage.data.message}</h3>
-          :
-          <h3>LOADING</h3>}</div>
-      </header>
-    </div>
+        <h1>Hello World!</h1>
+      </div>
   );
 }
 
