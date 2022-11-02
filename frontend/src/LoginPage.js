0693bedca1061955ac8494525ad960914b7edc13
xProioxis <christionb@hotmail.com>
2022-11-01 22:03:05 -0500

    Restablished Some of Tweaks to Website

---
+++ frontend/src/LoginPage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom'
+import './LoginPage.css';
+import './Login_SignUp_box.css'
+
+const LoginPage = () => {
+
+  return (
+    <div className= "mainHead">
+        <div className="container">
+          <div className="login_section">
+            <form className="form " id="login">
+              <h1 className="form__title">Login</h1>
+                <div className="form__message form__message--error">Incorrect username/password combination.</div>
+                <div className="form__input-group">
+                  <input type="text" className="form__input" autofocus placeholder="Username or email"></input>
+                  <div className="form__message form__input-error-message">This is an error message</div>
+                </div>
+                <div className="form__input-group">
+                  <input type="password" className="form__input" autofocus placeholder="Password"></input>
+                  <div className="form__input-error-message"></div>
+                </div>
+              <button className="form__button" type="submit">Submit</button>
+              <p class="form__text">
+                <a href="#" className="form__link">Forgot your password?</a>
+              </p>
+              <p className="form__text">
+                <a className="form__link" href=" ./signup">Dont have an account? Create account.</a>
+              </p>
+            </form>
+          </div>
+        </div>
+    </div>
+    
+    
+  );
+}
+
+export default LoginPage;
