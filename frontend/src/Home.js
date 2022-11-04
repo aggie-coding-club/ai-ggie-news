de447bebaaf0189103881368660e25cafd702299
alimmomin270@gmail.com <53840493+AlimMomin3735@users.noreply.github.com>
2022-11-04 10:38:17 -0500

    Updating UI
    Updated so body margins aren't shown + disabled scroll on website + added icon to top right

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import "./Home.css";
 function Home(){
     return(
-        <div>
+        <div className="Frame">
             <h1>AI-ggie News</h1>
         </div>
     );
