bb39c558b1328671bdb9cf2d63301d52359078d4
alimmomin270@gmail.com <53840493+AlimMomin3735@users.noreply.github.com>
2022-11-09 17:22:51 -0600

    Added UI for Home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,9 +1,50 @@
 import React from 'react'
 import "./Home.css";
+import WhiteIcon from './images/aiggieNewsWhiteIcon.png'
+
 function Home(){
     return(
         <div className="Frame">
-            <h1>AI-ggie News</h1>
+            <div className="Artcl_box_body">
+                <div className="ButtonList">
+                    <div className="ArticleContent">
+                        <button className="article">
+                            <h1 className="ArticleTitle">Article 1</h1>
+                            <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                            <img className="Article_image" src={WhiteIcon} alt="aiggieNews" />
+                        </button>
+                    </div>
+                    <button className="article">
+                        <h1 className="ArticleTitle">Article 2</h1>
+                        <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                    </button>
+                    <button className="article">
+                        <h1 className="ArticleTitle">Article 3</h1>
+                        <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                    </button>
+                    <button className="article">
+                        <h1 className="ArticleTitle">Article 4</h1>
+                        <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                    </button>
+                    <button className="article">
+                        <h1 className="ArticleTitle">Article 5</h1>
+                        <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                    </button>
+                    <button className="article">
+                        <h1 className="ArticleTitle">Article 6</h1>
+                        <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                    </button>
+                    <button className="article">
+                        <h1 className="ArticleTitle">Article 7</h1>
+                        <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                    </button>
+                    <button className="article">
+                        <h1 className="ArticleTitle">Article 8</h1>
+                        <p1 className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p1>
+                    </button>
+
+                </div>
+            </div>
         </div>
     );
 }
--- frontend/src/Preferences.js
+++ frontend/src/Preferences.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom'
+import "./Preferences.css";
 
 
 const Preferences = () => {
 
   return (
-        <ul>
-            <li>Sports</li>
-            <li>Business</li>
-        </ul>
-    
+        <div className="MainFrame">
+          <div className="SportsSection">
+            <h1>Sports</h1>
+            <button className="Button_select"> Basketball </button>
+            <button className="Button_select"> Football </button>
+            <button className="Button_select"> Baseball </button>
+            <button className="Button_select"> Track and Feild </button>
+            <button className="Button_select"> Golf </button>
+            <button className="Button_select"> Swim </button>
+          </div>
+          <div className="BusinessSection">
+            <h1>Business</h1>
+            <button className="Button_select"> Entrepenuarship </button>
+          </div>
+        </div>
 
 
   );
