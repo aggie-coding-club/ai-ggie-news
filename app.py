267f3a2e608030004ba515ac589fae6d778d427c
alimmomin270@gmail.com <53840493+AlimMomin3735@users.noreply.github.com>
2022-11-11 21:05:37 -0600

    First attempt to implement backend to home page

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_restful import Api, Resource, reqparse
 from api.HelloApiHandler import HelloApiHandler
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-# CORS(app) #comment this on deployment
+CORS(app) #comment this on deployment
 api = Api(app)
 
 @app.route("/", defaults={'path':''})
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,22 +1,56 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useState } from 'react';
 import "./Home.css";
 import WhiteIcon from './images/aiggieNewsWhiteIcon.png'
 
 
 function Home(){
+    const [profileData, setProfileData] = useState(null)
+ 
+   function getData() {
+     axios({
+       method: "GET",
+       url:"/articles",
+     })
+     .then((response) => {
+         console.log("Info recieved")
+         
+       const res =response.data
+       console.log(res.name)
+       setProfileData(({
+         profile_name: res.title,
+         paragraphCont: res.content}))
+     }).catch((error) => {
+       if (error.response) {
+         console.log(error.response)
+         console.log(error.response.status)
+         console.log(error.response.headers)
+         }
+     })}
+     getData()
     return(
         <div className="Frame">
             <div className="Artcl_box_body">
                 <div className="ButtonList">
+                    {/* new line start*/}
+                    {profileData && <div>
+                    <p>Profile name: {profileData.profile_name}</p>
+                    <p>About me: {profileData.paragraphCont}</p>
+                    </div>
+                    }
+                    {/* end of new line */}
                     <div className="ArticleContent">
                         <button className="article">
-                            <div className="articleText">
-                                <h1 className="ArticleTitle">Article 1</h1>
-                                <p className="ArticleBodyPara">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non sodales diam, a convallis velit. Nulla facilisi. Morbi quam nisi, vestibulum hendrerit bibendum sed, semper non nisi. In ultricies ut arcu eget elementum. Proin maximus ipsum neque, ut vestibulum velit efficitur sed. Morbi tristique erat id tellus lobortis, at sollicitudin nisi sagittis. Morbi elementum quam eget sapien tempus luctus. Nullam et nisl nisi. Proin id ante egestas felis viverra facilisis accumsan ac arcu. Ut ut lectus nisl.</p>
-                            </div>
-                            <div className="ArticleImage_Content">
-                                <img className="Article_image_direct" src={WhiteIcon} alt="aiggieNews" />
-                            </div>
+                            {profileData && <div>
+                                <div className="articleText">
+                                    <h1 className="ArticleTitle">{profileData.profile_name}</h1>
+                                    <p className="ArticleBodyPara">{profileData.paragraphCont}</p>
+                                </div>
+                                <div className="ArticleImage_Content">
+                                    <img className="Article_image_direct" src={WhiteIcon} alt="aiggieNews" />
+                                </div>
+                            </div>}
                         </button>
                     </div>
                     <div className="ArticleContent">
