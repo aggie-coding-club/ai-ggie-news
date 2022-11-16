bbd2dfa56234f7b03729e3c43bb1cb4bb03c2b15
xProioxis <christionb@hotmail.com>
2022-11-16 16:22:30 -0600

    Development of Integration of Model

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 from flask import Flask, send_from_directory
 from flask_cors import CORS  # comment this on deployment
 from flask_restful import Api, Resource, reqparse
+import models
 
 from api.HelloApiHandler import HelloApiHandler
 
@@ -13,16 +14,17 @@ api = Api(app)
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
+
+
 @app.route('/articles')
 def articles():
+    # model = models.init_model()
+    print(model)
     articles = pd.read_csv("test_info.csv")
     article_titles = articles['title'].values
-    #article_title = "Hello World"
     content_body = articles['content'].values
-    #content_body= "The world is happy."
     link_ref = articles['link'].values
     img_ref = articles['image'].values
-    #link_ref = "https://youtube.com"
     return {"title1": article_titles[0], "content1": content_body[0], "link1": link_ref[0], "image1":img_ref[0],
     "title2": article_titles[1], "content2": content_body[1], "link2": link_ref[1], "image2":img_ref[1],
     "title3": article_titles[2], "content3": content_body[2], "link3": link_ref[2], "image3":img_ref[2],
